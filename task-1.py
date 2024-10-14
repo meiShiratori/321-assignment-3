c2834718a13b66b270781b053e91f9bc0de14d53
7meiSh <7mei.sh@gmail.com>
2024-10-14 16:00:10 -0700

    first commit

---
+++ task-1.py
@@ -0,0 +1,20 @@
+def generate_private_key():
+    return None
+
+def compute_public_key():
+    return None
+
+def compute_shared_secret():
+    return None
+
+def derive_key():
+    return None
+
+def encrypt_message():
+    return None
+
+def decrypt_message():
+    return None
+
+def diffie_hellman_protocol():
+    return None
